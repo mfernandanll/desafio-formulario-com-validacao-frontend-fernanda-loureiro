b3da96f30ea49f9644ca4f96b6026d90f95c0ccf
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-10-16 13:57:26 -0400

    feat: install and configure Tailwind CSS for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-
-import './App.css'
-
-function App() {
-
+export default function App() {
   return (
     <>
-      <main>
+      <main className='h-full min-h-screen text-red-700'>
         <h1>Seja bem vindo! :)</h1>
         <p>Terceiro Desafio: Formulário de Cadastro de Membros</p>
       </main>
     </>
   );
 }
-
-export default App
