ab499873e73e7f7b4b729e04faa939205631fea6
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-10-19 20:24:49 -0400

    feat: implement header element and styling application layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 export default function App() {
   return (
-    <>
-      <main className='h-full min-h-screen text-red-700'>
-        <h1>Seja bem vindo! :)</h1>
-        <p>Terceiro Desafio: Formulário de Cadastro de Membros</p>
-      </main>
-    </>
+    <div className="min-h-screen w-full flex bg-zinc-100 text-zinc-800 font-sans">
+      <div className="flex flex-col items-center mx-auto py-4 px-12 my-12 bg-zinc-50 rounded-xl">
+        <header className="container py-12 px-4 text-start">
+          <h1 className="text-4xl font-bold">Formulário Fusion</h1>
+          <p className="text-xl mt-2">Preencha o formulário com seus dados para se cadastrar como membro</p>
+        </header>
+        <main className="">
+        </main>
+      </div>
+    </div>
   );
 }
